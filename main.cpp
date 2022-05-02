9ea6cf110a100fd56457d5c80bc42b68b95cde6f
nourelshaer <nour.elshaer2003@gmail.com>
2022-05-02 02:31:52 +0200

    Added functions 6-10

--- main.cpp
+++ main.cpp
@@ -254,19 +254,113 @@ void encrypt(){}
 void decrypt(){}
 
 //_____________________________
-void merge(){}
+void merge(){
+    fstream file2;
+    char filename2[100];
+    bool exist = false;
+    while (exist == false) {
+        cout << "Please enter the txt-file name you want to merge with:\n";
+        // taking the File name from user
+        cin >> filename2;
+
+        // adding the .txt extension
+        strcat(filename2, ".txt");
+
+        // trying to open the file in read-only mode (so I can check if it exists)
+        file2.open(filename2);
+
+        if(file2.fail()){ // checking if a file doesn't exist
+            cout << "Invalid File Name. Please try again.\n";
+        }
+        else{
+            file.open(filename, ios::app);
+            file << file2.rdbuf();
+            cout << "Files merged successfully!\n";
+            exist = true;
+        }   
+    }
+    file.close();
+    file2.close();
+}
 
 //_____________________________
-void count_words(){}
+void count_words(){
+    file.open(filename, ios::in);
+    string word;
+    int word_counter = 0;
+    cin.ignore();
+    cin.sync();
+    while (!file.eof()) {
+        file >> word;
+        ++word_counter;
+    }
+    cout << "Number of words in the file is: " << word_counter << endl;
+    file.close();
+}
 
 //_____________________________
-void count_chars(){}
+void count_chars(){
+    file.open(filename, ios::in);
+    string word;
+    int char_counter = 0;
+    cin.ignore();
+    cin.sync();
+    while (!file.eof()) {
+        file >> word;
+        char_counter += word.length();
+    }
+    cout << "Number of characters in the file is: " << char_counter << endl;
+    file.close();
+}
 
 //_____________________________
-void count_lines(){}
+void count_lines(){
+    file.open(filename, ios::in);
+    char word[100];
+    int line_counter = 0;
+    cin.ignore();
+    cin.sync();
+    while (!file.eof()) {
+        ++line_counter;
+        file.getline(word, 100); // use \n as a delimiter
+    }
+    cout << "Number of lines in the file is: " << line_counter << endl;
+    file.close();
+}
 
 //_____________________________
-void search_word(){}
+void search_word(){
+    file.open(filename, ios::in);
+
+    cout << "\nEnter the word to search if it exists in the file\n>>";
+
+    // entering the word to search for it
+    string word;
+    bool exist = false;
+    cin.ignore();
+    cin.sync();
+    getline(cin, word);
+
+    string temp_word;
+
+    while(!file.eof()){ //while didn't reach End Of File
+        // putting every word in a string
+        file >> temp_word;
+
+        // checking if this word is the word we're looking for
+        if(tolower(temp_word) == tolower(word)) {
+            exist = true;
+            break;
+        }
+    }
+    if(exist == false) {
+        cout << "Word was not found in the file!\n";
+    }
+    else {
+        cout << "Word was found in the file!\n";
+    }
+    file.close();
+}
 
 //_____________________________
 void word_freq(){
